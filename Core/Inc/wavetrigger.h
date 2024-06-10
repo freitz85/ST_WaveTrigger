09fce8b9e1335903a8753486e7f5082082865a84
Florian Reitz <freitz85@gmail.com>
2024-06-10 13:17:05 +0200

    Track methods

--- Core/Inc/wavetrigger.h
+++ Core/Inc/wavetrigger.h
@@ -2,6 +2,7 @@
 #define WAVETRIGGER_H
 
 #include <cstdint>
+//#include "stm32f4xx_hal_uart.h"
 
 // Message format: ( SOM1, SOM2, length, message code, data * n , EOM )
 //	where SOM1=0xf0, SOM2=0xaa,  EOM=0x55
@@ -10,28 +11,28 @@
 #define CMD_GET_SYS_INFO        0x02
 #define CMD_TRACK_CONTROL       0x03
 #define CMD_STOP_ALL          	0x04
-//#define CMD_MASTER_VOLUME       0x05
+#define CMD_MASTER_VOLUME       0x05
 #define CMD_TRACK_VOLUME        0x08
-//#define CMD_AMP_POWER         	0x09
+#define CMD_AMP_POWER         	0x09
 #define CMD_TRACK_FADE          0x0a
-//#define CMD_RESUME_ALL_SYNC     0x0b
+#define CMD_RESUME_ALL_SYNC     0x0b
 #define CMD_SAMPLERATE_OFFSET   0x0c
-//#define CMD_TRACK_CONTROL_EX    0x0d
+#define CMD_TRACK_CONTROL_EX    0x0d
 #define CMD_SET_REPORTING       0x0e
-//#define CMD_SET_TRIGGER_BANK    0x0f
+#define CMD_SET_TRIGGER_BANK    0x0f
 
-//#define TRK_PLAY_SOLO         	0
+#define TRK_PLAY_SOLO         	0
 #define TRK_PLAY_POLY         	1
-//#define TRK_PAUSE           	2
-//#define TRK_RESUME            	3
+#define TRK_PAUSE           	2
+#define TRK_RESUME            	3
 #define TRK_STOP            	4
 #define TRK_LOOP_ON           	5
 #define TRK_LOOP_OFF          	6
-//#define TRK_LOAD            	7
+#define TRK_LOAD            	7
 
 #define RSP_VERSION_STRING      0x81
 #define RSP_SYSTEM_INFO         0x82
-//#define RSP_STATUS            	0x83
+#define RSP_STATUS            	0x83
 #define RSP_TRACK_REPORT        0x84
 
 #define MAX_MESSAGE_LEN         32
@@ -42,9 +43,20 @@
 #define SOM2  					0xaa
 #define EOM   					0x55
 
+#define NUM_VOICES				8
+
 
 enum PARSER_STATE { WAIT_SOM1, WAIT_SOM2, GET_LENGTH, GET_CMD, GET_DATA, WAIT_EOM };
-enum PARSER_RESULT { IDLE, PARSING, ERROR, FINISHED };
+enum PARSER_RESULT { IDLE, PARSING, PARSER_ERROR, FINISHED };
+
+struct voice
+{
+	uint16_t trackNumber;
+	uint32_t offset;
+	uint8_t playFlag;
+	uint8_t lockFlag;
+	int16_t gain;
+};
 
 class Wavetrigger
 {
@@ -52,23 +64,39 @@ private:
 	enum PARSER_STATE m_parserState;
 	enum PARSER_RESULT m_parserResult;
 	uint8_t m_command;
-	uint8_t m_length;
-	uint8_t m_dataIndex;
-	uint8_t m_data[255];
+	uint8_t m_rxLength;
+	uint8_t m_txLength;
+	uint8_t m_rxDataIndex;
+	uint8_t m_rxData[255];
+	uint8_t m_txData[255];
+	uint16_t m_numTracks;
+	uint8_t m_reportingEnabled;
+	voice voices[NUM_VOICES];
+
+	//UART_HandleTypeDef* m_pUart;
 
 public:
 	Wavetrigger() { Reset(); }
 	void Reset() {
 		m_parserState = WAIT_SOM1;
 		m_parserResult = IDLE;
-		m_length = 0;
-		m_dataIndex = 0;
+		m_rxLength = 0;
+		m_txLength = 0;
+		m_rxDataIndex = 0;
+		m_txData[0] = SOM1;
+		m_txData[1] = SOM2;
 	}
 
 	PARSER_RESULT Parse(uint8_t data);
 	void DoCommand();
+	void SetNumTracks(uint16_t numTracks) { m_numTracks = numTracks; }
+	uint8_t GetReportingEnabled() { return m_reportingEnabled; }
+	void SendTrackReport(uint16_t trackNumber, uint8_t voice, uint8_t state);
 
 private:
+	voice* GetFreeVoice();
+	voice* GetVoice(uint16_t trackNumber);
+	void SendResponse(uint8_t cmd, void* pData, uint8_t dataSize);
 	void CmdGetVersion();
 	void CmdGetSysInfo();
 	void CmdTrackControl();
--- Core/Src/wavetrigger.cpp
+++ Core/Src/wavetrigger.cpp
@@ -1,5 +1,5 @@
 #include "wavetrigger.h"
-
+#include <cstring>
 
 PARSER_RESULT Wavetrigger::Parse(uint8_t data) {
 
@@ -13,7 +13,7 @@ PARSER_RESULT Wavetrigger::Parse(uint8_t data) {
 		}
 		else
 		{
-			m_parserResult = ERROR;
+			m_parserResult = PARSER_ERROR;
 		}
 		break;
 	case WAIT_SOM2:
@@ -23,11 +23,11 @@ PARSER_RESULT Wavetrigger::Parse(uint8_t data) {
 		}
 		else
 		{
-			m_parserResult = ERROR;
+			m_parserResult = PARSER_ERROR;
 		}
 		break;
 	case GET_LENGTH:
-		m_length = data - 4;
+		m_rxLength = data - 4;
 		m_parserState = GET_CMD;
 		break;
 	case GET_CMD:
@@ -35,14 +35,14 @@ PARSER_RESULT Wavetrigger::Parse(uint8_t data) {
 		m_parserState = GET_DATA;
 		break;
 	case GET_DATA:
-		m_data[m_dataIndex] = data;
-		if(m_dataIndex == m_length + 1)
+		m_rxData[m_rxDataIndex] = data;
+		if(m_rxDataIndex == m_rxLength + 1)
 		{
 			m_parserState = WAIT_EOM;
 		}
 		else
 		{
-			m_dataIndex++;
+			m_rxDataIndex++;
 		}
 		break;
 	case WAIT_EOM:
@@ -52,11 +52,11 @@ PARSER_RESULT Wavetrigger::Parse(uint8_t data) {
 		}
 		else
 		{
-			m_parserResult = ERROR;
+			m_parserResult = PARSER_ERROR;
 		}
 		break;
 	default:
-		m_parserResult = ERROR;
+		m_parserResult = PARSER_ERROR;
 	}
 
 
@@ -68,54 +68,130 @@ void Wavetrigger::DoCommand()
 	switch(m_command)
 	{
 	case CMD_GET_VERSION:
-
+		CmdGetVersion();
 		break;
 	case CMD_GET_SYS_INFO:
-
+		CmdGetSysInfo();
 		break;
 	case CMD_TRACK_CONTROL:
-
+		CmdTrackControl();
 		break;
 	case CMD_STOP_ALL:
-
+		CmdStopAll();
 		break;
 	case CMD_TRACK_VOLUME:
-
+		CmdTrackVolume();
 		break;
 	case CMD_TRACK_FADE:
-
+		CmdTrackFade();
 		break;
-//#define CMD_RESUME_ALL_SYNC     0x0b
-#define CMD_SAMPLERATE_OFFSET   0x0c
-//#define CMD_TRACK_CONTROL_EX    0x0d
-#define CMD_SET_REPORTING       0x0e
-	void CmdGetVersion();
-	void CmdGetSysInfo();
-	void CmdTrackControl();
-	void CmdStopAll();
-	void CmdTrackVolume();
-	void CmdTrackFade();
-	void CmdSamplerateOffset();
-	void CmdSetReporting();
+	case CMD_SAMPLERATE_OFFSET:
+		CmdSamplerateOffset();
+		break;
+	case CMD_SET_REPORTING:
+		CmdSetReporting();
+		break;
 	default:
 		// do nothing
+		break;
 	}
+
+
+}
+
+void Wavetrigger::SendTrackReport(uint16_t trackNumber, uint8_t voice, uint8_t state)
+{
+	char info[4];
+	info[0] = (uint8_t)m_numTracks;
+	info[1] = m_numTracks>>8;
+	info[2] = voice;
+	info[3] = state;
+
+	SendResponse(RSP_TRACK_REPORT, info, sizeof(info));
+}
+
+voice* Wavetrigger::GetFreeVoice()
+{
+	voice* pVoice = nullptr;
+	for(uint8_t n=0; n<NUM_VOICES; n++)
+	{
+		if((voices[n].playFlag == 0) && (voices[n].playFlag == 0))
+		{
+			pVoice = &voices[n];
+			break;
+		}
+	}
+	return pVoice;
+}
+
+voice* Wavetrigger::GetVoice(uint16_t trackNumber)
+{
+	voice* pVoice = nullptr;
+	for(uint8_t n=0; n<NUM_VOICES; n++)
+	{
+		if(voices[n].trackNumber == trackNumber)
+		{
+			pVoice = &voices[n];
+			break;
+		}
+	}
+	return pVoice;
+}
+
+void Wavetrigger::SendResponse(uint8_t cmd, void* pData, uint8_t dataSize)
+{
+	m_txLength = dataSize + 5;
+	m_txData[2] = m_txLength;
+	m_txData[3] = cmd;
+	memcpy(m_txData+4, pData, dataSize);
+	m_txData[dataSize+5] = EOM;
+
+	//HAL_UART_Transmit(m_pUart, m_txData, m_txLength, 0);
 }
 
 void Wavetrigger::CmdGetVersion(){
-
+	const char* version = "StWavetrigger         ";
+	SendResponse(RSP_VERSION_STRING, const_cast<char*>(version), strlen(version));
 }
 
 void Wavetrigger::CmdGetSysInfo(){
+	char info[3];
+	info[0] = NUM_VOICES;
+	info[1] = (uint8_t)m_numTracks;
+	info[2] = m_numTracks>>8;
 
+	SendResponse(RSP_SYSTEM_INFO, info, sizeof(info));
 }
 
 void Wavetrigger::CmdTrackControl(){
+	uint16_t trackNumber = m_rxData[1] + (m_rxData[2] << 8);
+	uint8_t lockFlag = m_rxData[3];
 
+	switch(m_rxData[0])
+	{
+	case TRK_PLAY_SOLO:
+		break;
+	case TRK_PLAY_POLY:
+		break;
+	case TRK_PAUSE:
+		break;
+	case TRK_RESUME:
+		break;
+	case TRK_STOP:
+		break;
+	case TRK_LOOP_ON:
+		break;
+	case TRK_LOOP_OFF:
+		break;
+	case TRK_LOAD:
+		break;
+	default:
+		break;
+	};
 }
 
 void Wavetrigger::CmdStopAll(){
-
+	// for each track in list, disable play flag
 }
 
 void Wavetrigger::CmdTrackVolume(){
@@ -131,5 +207,5 @@ void Wavetrigger::CmdSamplerateOffset(){
 }
 
 void Wavetrigger::CmdSetReporting(){
-
+	m_reportingEnabled = m_rxData[0];
 }
